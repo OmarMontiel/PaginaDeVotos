bfbe5fc9c588f29955f4ee32c5c1eeb80f61408d
Omar ALEJANDRO <omar.montiel.vazquez@gmail.com>
2017-06-17 16:59:00 -0500

    2.0

--- Contorlador/vista.js
+++ Contorlador/vista.js
@@ -1,23 +1,50 @@
 var voto = " ";
-function f_pri() {
-  document.getElementById('parrafo').innerHTML="voto pri";
-  voto="pri";
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    }
-  };
-  xmlhttp.open("GET","ConecionBaseDatos.php?q="+voto,true);
-  xmlhttp.send();
-  alert("ff");
+
+function f_nulo(){
+        voto = "nulo";
+        var xmlhttp = new XMLHttpRequest();
+	      xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+
+            }
+        };
+    xmlhttp.open("GET", "../Modelo/BaseDatos.php?q=" + voto, true);
+    xmlhttp.send();
+    window.location="../MVCVisual/Vista.html";
 }
 
-function f_pan() {
-  document.getElementById('parrafo').innerHTML="voto pan";
+function f_pri(){
+         voto = "pri";
+	       var xmlhttp = new XMLHttpRequest();
+	       xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+            }
+        };
+    xmlhttp.open("GET", "../Modelo/BaseDatos.php?q=" + voto, true);
+    xmlhttp.send();
+    window.location="../MVCVisual/Vista.html";
 }
-function f_prd() {
-  document.getElementById('parrafo').innerHTML="voto prd";
+function f_prd(){
+  voto = "prd";
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+            }
+        };
+    xmlhttp.open("GET", "../Modelo/BaseDatos.php?q=" + voto, true);
+    xmlhttp.send();
+    window.location="../MVCVisual/Vista.html";
 }
-function f_nulo() {
-  document.getElementById('parrafo').innerHTML="voto nulo";
+
+function f_pan(){
+  voto = "pan";
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+
+            }
+        };
+    xmlhttp.open("GET", "../Modelo/BaseDatos.php?q=" + voto, true);
+    xmlhttp.send();
+    window.location="../MVCVisual/Vista.html";
 }
--- Modelo/BaseDatos.php
+++ Modelo/BaseDatos.php
@@ -1,32 +1,45 @@
 
 <?php
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$database = "sistemasdevoto";
+	$q = $_GET['q'];
+
+	$connection = mysql_connect($host, $user, $pass) or die('Error en la conexion: ' . mysql_error());
+	mysql_select_db($database) or die('Error en conexion db: ' . mysql_error());
+
+	if($q == "nulo"){
+		f_nulo();
+	}elseif ($q == "pan") {
+		f_pan();
+	}elseif ($q == "pri") {
+		f_pri();
+	}elseif ($q == "prd") {
+		f_prd();
+	}
 
 
-
-	$connection = mysql_connect("localhost","root","","sistemasdevoto")or die('Error en la conexion: ' . mysql_error());
-	$db = mysql_select_db("sistemasdevoto",$connection) or die(" <h2> error de conecion <h2>");
 
 
 	function f_pri(){
-		$query = "SELECT N_votos FROM votos WHERE ID= 1 ";
+		$query = "SELECT N_votos FROM votos WHERE ID=1 ";
 		$result = mysql_query($query,$connection) or die('Error en la consulta: ' . mysql_error());
 		$row = mysql_fetch_array($result);
-		echo "string";
 		$votosPRI = $row['N_votos'];
 		$votosPRI = $votosPRI + 1;
-		UPDATE `votos` SET `N_votos` = '8' WHERE `votos`.`ID` = 1;
+		$query = "UPDATE votos SET N_votos='$votosPRI' WHERE ID=1";
 		$result = mysql_query($query) or die('Error en la consulta: ' . mysql_error());
 
-		echo " alert("hola") ";
 	}
 
   function f_pan(){
-		$query = "SELECT N_votos FROM votos WHERE Nombre= 'pri' ";
+		$query = "SELECT N_votos FROM votos WHERE  ID=2 ";
 		$result = mysql_query($query) or die('Error en la consulta: ' . mysql_error());
 		$row = mysql_fetch_array($result);
 		$votosPAN = $row['N_votos'];
 		$votosPAN = $votosPAN + 1;
-		$query = "UPDATE votos SET N_votos='$votosPAN' WHERE id=2";
+		$query = "UPDATE votos SET N_votos='$votosPAN' WHERE ID=2";
 		$result = mysql_query($query) or die('Error en la consulta: ' . mysql_error());
 	}
   function f_prd(){
@@ -35,7 +48,7 @@
 		$row = mysql_fetch_array($result);
 		$votosPRD = $row['N_votos'];
 		$votosPRD = $votosPRD + 1;
-		$query = "UPDATE votos SET N_votos='$votosPRD' WHERE id=3";
+		$query = "UPDATE votos SET N_votos='$votosPRD' WHERE ID=3";
 		$result = mysql_query($query) or die('Error en la consulta: ' . mysql_error());
 	}
   function f_nulo(){
@@ -44,8 +57,9 @@
 		$row = mysql_fetch_array($result);
 		$votosNulo = $row['N_votos'];
 		$votosNulo = $votosNulo + 1;
-		$query = "UPDATE votos SET N_votos='$votosNulo' WHERE id=4";
+		$query = "UPDATE votos SET N_votos='$votosNulo' WHERE ID=4";
 		$result = mysql_query($query) or die('Error en la consulta: ' . mysql_error());
+		return  $votosNulo;
 	}
 
   mysql_close($connection);
