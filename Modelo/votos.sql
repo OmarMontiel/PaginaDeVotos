885c1238c49a54d4b24cd52a084bd6c0d45b0d1b
Omar ALEJANDRO <omar.montiel.vazquez@gmail.com>
2017-06-17 17:24:17 -0500

    2.0 mas base de datos correncion

--- Modelo/votos.sql
+++ Modelo/votos.sql
@@ -1,24 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 18-06-2017 a las 00:00:47
--- Versión del servidor: 10.1.16-MariaDB
--- Versión de PHP: 5.6.24
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `sistemasdevoto`
---
+CREATE DATABASE IF NOT EXISTS `sistemasdevoto` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `sistemasdevoto`;
 
 -- --------------------------------------------------------
 
@@ -38,7 +20,7 @@ CREATE TABLE `votos` (
 
 INSERT INTO `votos` (`ID`, `Nombre`, `N_votos`) VALUES
 (1, 'PRI', 0),
-(2, 'PAN', 14),
+(2, 'PAN', 17),
 (3, 'PRD', 6),
 (4, 'NULO', 7);
 
@@ -51,7 +33,6 @@ INSERT INTO `votos` (`ID`, `Nombre`, `N_votos`) VALUES
 --
 ALTER TABLE `votos`
   ADD PRIMARY KEY (`ID`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Base de datos: `test`
+--
